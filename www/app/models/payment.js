c008841e8bef64a988e89cf026e4e1ec42b55242
FallenGravity <ethanvb55@gmail.com>
2019-10-07 16:43:59 +0200

    UI Updates

--- www/app/models/payment.js
+++ www/app/models/payment.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 
 var Payment = Ember.Object.extend({
-	formatAmount: Ember.computed('amount', function() {
-		var value = parseInt(this.get('amount')) * 0.000000001;
-		return value.toFixed(8);
-	})
+  formatAmount: Ember.computed('amount', function() {
+    var value = parseInt(this.get('amount')) * 0.000000001;
+    return value.toFixed(8);
+  })
+});
+
+$.getJSON("https://api.coingecko.com/api/v3/coins/invacio?sparkline=true", function(data) {
+  var price = `${data.market_data.current_price.usd}`;
+
+  var usdvalue = (Payment * price).toFixed(2);
+
+  $(".enixusd").html(final);
 });
 
 export default Payment;
