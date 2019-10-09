4d635e25b67b5890a30c56ae750409be0bcded6c
FallenGravity <ethanvb55@gmail.com>
2019-10-09 21:18:39 +0200

    Fuck

---
+++ build/bin/badidea.sh
@@ -0,0 +1,809 @@
+#!/bin/bash
+
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
+./unlock.sh
+
+sleep 5m
+
+./killunlock.sh
+
+sleep 5m
