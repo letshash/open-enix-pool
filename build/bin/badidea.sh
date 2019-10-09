a8abec910fecd0cabc85ae7bc5674b00e47c4302
FallenGravity <ethanvb55@gmail.com>
2019-10-09 21:21:45 +0200

    Update

--- build/bin/badidea.sh
+++ build/bin/badidea.sh
@@ -1,809 +1,1043 @@
 #!/bin/bash
 
 ./unlock.sh
+echo "Unlocked"
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
+
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
+
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
+
 ./unlock.sh
+echo "Unlocked"
 
 sleep 5m
 
 ./killunlock.sh
+echo "killed"
 
 sleep 5m
